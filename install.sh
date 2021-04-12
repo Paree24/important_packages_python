ec778ec136113f51fec90c60d44111d9839b56a4
Pareekshith Katti <37375677+Paree24@users.noreply.github.com>
2021-04-12 13:19:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt install jupyter-core
 pip3 install -r requirements.txt
 pip3 install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
+sudo  update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
