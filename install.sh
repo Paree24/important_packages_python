13484d7d44ad317e2ac04355c57aba01560b7372
Pareekshith Katti <37375677+Paree24@users.noreply.github.com>
2021-07-21 12:54:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ pip3 install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
 sudo  update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+!jupyter nbextension install — py luxwidget
+!jupyter nbextension enable — py luxwidget
