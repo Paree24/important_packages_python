ad940899799978fd97d6100a7e204285940ea58c
Pareekshith Katti <37375677+Paree24@users.noreply.github.com>
2021-07-21 12:32:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ pip3 install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
 sudo  update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+python -m mitoinstaller install
